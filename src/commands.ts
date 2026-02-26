262161eeac5894332324d02e556fde7e2a631daa
RiMaBo <rimabo@gmail.com>
2026-02-25 17:48:18 -0700

    CH3L1: Pokedex

--- src/commands.ts
+++ src/commands.ts
@@ -5,6 +5,7 @@ import { commandMapForward, commandMapBack } from "./command_map.js"
 import { commandExplore } from "./command_explore.js"
 import { commandCatch } from "./command_catch.js"
 import { commandInspect } from "./command_inspect.js"
+import { commandPokedex } from "./command_pokedex.js"
 
 export function getCommands(): Record<string, CLICommand> {
     return {
@@ -37,6 +38,11 @@ export function getCommands(): Record<string, CLICommand> {
             name: "inspect <pokemon name>",
             description: "Inspect a caught pokemon.",
             callback: commandInspect,
+        },
+        pokedex: {
+            name: "pokedex",
+            description: "List all caught pokemon.",
+            callback: commandPokedex,
         },
         exit: {
             name: "exit",
